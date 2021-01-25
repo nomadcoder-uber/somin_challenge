2a39bc504c7308da1ea439927efeaa47ed6290e0
somin <qkrthals0524@naver.com>
2021-01-25 14:17:25 +0900

    React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
+import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
 export default function App() {
-  return <LoggedOutRouter />;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,20 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: "https://codesandbox.io/s/trusting-architecture-s6hri",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
